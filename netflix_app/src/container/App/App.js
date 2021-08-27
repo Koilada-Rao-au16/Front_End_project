68aa171c670dd24aaa90b39a8f71a1e89648c810
Developer <developer@Administrators-MacBook-Pro.local>
2021-08-27 18:08:53 +0530

    netflix app array added in component

--- netflix_app/src/container/App/App.js
+++ netflix_app/src/container/App/App.js
@@ -1,38 +1,56 @@
 import React from 'react';
 import Card from '../../components/Card';
 import image from '../../components/images/bhaskar1.jpeg';
+import cdata from '../../../src/cdata';
 
+// console.log(cdata[1].title);
 const App = () => {
   return (
-    <div>
+    <>
+      <h1 className='heading_style'>List of top Netflix series in 2021</h1>
       <Card
-        imgSrc='https://wallpapercave.com/wp/wp4056410.jpg'
-        title=' A Netflix Original Series'
-        sname='DARK'
-        link='https://www.netflix.com/in/title/80990668?source=35'
+        imgSrc={cdata[0].imgSrc}
+        title={cdata[0].title}
+        link={cdata[0].link}
+        sname={cdata[0].sname}
       />
-      <Card
-        style={{ height: '900px', width: '900px' }}
+      { <Card
         link=' https://www.primevideo.com/?ref_=dvm_pds_amz_in_as_s_g_18_mkw_sHPVHrkzE-dc&mrntrk=pcrid_386559717075_slid__pgrid_82649959887_pgeo_1007743_x__ptid_kwd-816922922808'
         imgSrc={image}
         sname='BHASKAR'
-        date=''
+        title="Own Series"
+      /> }
+      <Card
+        imgSrc={cdata[1].imgSrc}
+        title={cdata[1].title}
+        link={cdata[1].link}
+        sname={cdata[1].sname}
       />
       <Card
-        imgSrc="https://wallpapercave.com/wp/wp2497187.jpg"
-        imgSrc='https://c4.wallpaperflare.com/wallpaper/572/47/710/zipper-ragnarok-lightning-warrior-gladiator-hd-wallpaper-thumb.jpg'
-        title=' A Netflix Original Series'
-        sname='THOR'
-        link='https://www.netflix.com/in/title/80990668?source=35'
+        imgSrc={cdata[2].imgSrc}
+        title={cdata[2].title}
+        link={cdata[2].link}
+        sname={cdata[2].sname}
       />
       <Card
-        imgSrc='https://www.wallpapertip.com/wmimgs/84-843938_full-hd-1080p-iron-man.jpg'
-        
-        title=' A Netflix Original Series'
-        sname='IRON MAN'
-        link='https://www.netflix.com/in/title/80990668?source=35'
+        imgSrc={cdata[3].imgSrc}
+        title={cdata[3].title}
+        link={cdata[3].link}
+        sname={cdata[3].sname}
       />
-    </div>
+      <Card
+        imgSrc={cdata[4].imgSrc}
+        title={cdata[4].title}
+        link={cdata[4].link}
+        sname={cdata[4].sname}
+      />
+      <Card
+        imgSrc={cdata[5].imgSrc}
+        title={cdata[5].title}
+        link={cdata[5].link}
+        sname={cdata[5].sname}
+      />
+    </>
   );
 };
 
--- netflix_app/src/index.js
+++ netflix_app/src/index.js
@@ -2,6 +2,8 @@
 import React from 'react'
 import reactDom from 'react-dom'
 import App from './container/App/App'
+import './index.css'
+
 
 reactDom.render(
   <App/>,document.getElementById("root")
