e2265875c893826619609262d230cd04b374a209
Developer <developer@Administrators-MacBook-Pro.local>
2021-08-29 02:08:31 +0530

    time update when click on button with reactjs

---
+++ digitalclock/src/App.js
@@ -0,0 +1,14 @@
+
+import Timeupdate from "./components/Timeupdate";
+
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <Timeupdate/>
+    </div>
+  )
+}
+
+export default App
